ab784a58abd36095b2f473263580b170a02539d9
Gianmarco Polizia <gpolizia4@gmail.com>
2022-08-27 14:52:57 +0200

    modified script.js

--- rolls/static/js/script.js
+++ rolls/static/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("yeah yo yah");
-    let valueCounter = document.getElementById('counter').value;
+    let valueCounter = document.getElementById('counter').innerHTML;
+    // valueCounter = parseInt(valueCounter);
     const incrementBtn = document.getElementById('incrementBtn');
     const decrementBtn = document.getElementById('decrementBtn');
 
@@ -10,6 +11,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // });
 
     incrementBtn.addEventListener('click', () => {
+        console.log(valueCounter);
         valueCounter++;
+        document.getElementById('counter').innerHTML = valueCounter;
+        console.log(valueCounter);
     });
+
+    decrementBtn.addEventListener('click', () => {
+        if (valueCounter > 0) 
+        {
+            valueCounter--;
+        }
+        document.getElementById('counter').innerHTML = valueCounter;
+    })
 });
